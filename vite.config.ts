8d4bc301d6bb37f951552532160332fd991110fd
jutlyne <vocaoky290999@gmail.com>
2024-07-20 13:20:25 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,21 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import Pages from 'vite-plugin-pages'
+import generateSitemap from 'vite-plugin-pages-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   return {
-    plugins: [vue(), VueDevTools(), AutoImport({}), Components({})],
+    plugins: [
+      vue(),
+      VueDevTools(),
+      AutoImport({}),
+      Components({}),
+      Pages({
+        onRoutesGenerated: routes => (generateSitemap({ routes })),
+      })
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
