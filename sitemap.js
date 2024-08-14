210bcbca96c2a24a2bc17b98c191b88403ea7f3a
kyvc <kyvc.nta@gmail.com>
2024-08-14 11:07:50 +0700

    update

--- sitemap.js
+++ sitemap.js
@@ -1,12 +1,16 @@
 import { resolve } from 'path'
 import { simpleSitemapAndIndex, EnumChangefreq } from 'sitemap'
 
+const getCurrentDate = () => {
+  return new Date().toISOString()
+}
+
 simpleSitemapAndIndex({
   hostname: 'https://www.vocaoky.site',
   destinationDir: resolve('./public'),
   sourceData: [
-    { url: '/', changefreq: EnumChangefreq.DAILY },
-    { url: '/blogs', changefreq: EnumChangefreq.DAILY }
+    { url: '/', changefreq: EnumChangefreq.DAILY, lastmodISO: getCurrentDate(), priority: 0.7 },
+    { url: '/blogs', changefreq: EnumChangefreq.DAILY, lastmodISO: getCurrentDate(), priority: 0.7 }
   ],
   gzip: false
 })
